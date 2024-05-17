257d9f40218c23bf801d52eeeb7375058217a864
Nikolay <NikolayBuch3@gmail.com>
2024-05-17 15:47:41 +0300

    modal

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,27 +1,4 @@
-// 'use client';
 
-// import { useCallback, useState } from 'react';
-
-
-// const useModal = () => {
-
-//   const [isModalOpen, setModalOpen] = useState(false)
-//   const [modalData, setModalData] = useState({
-//     content: {},
-//     type: '',
-//   })
-//   const toggleModal = useCallback(
-//     (content = false) => {
-//       setModalOpen(!isModalOpen);
-//       if (content) {
-//         setModalData(content)
-//       }
-//     }, [isModalOpen])
-
-//   return { isModalOpen, toggleModal, modalData }
-// };
-
-// export default useModal;
 
 import { useContext } from 'react';
 
