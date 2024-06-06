73d667a4fb7a8146221af9a27a490418a9e59383
Nikolay <NikolayBuch3@gmail.com>
2024-06-06 11:17:16 +0300

    fix hover

--- src/components/common/NavTop/NavTop.jsx
+++ src/components/common/NavTop/NavTop.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
-import s from './NavTop.module.scss';
 import Nav from './Nav';
 import Container from 'components/Container';
 
+import s from './NavTop.module.scss';
 
 const HeaderTop = () => {
   return (
-      <Container size='normal' className={s.root}>
-      <Nav/>
-
-      </Container>
+    <Container size='normal' className={s.root}>
+      <Nav />
+    </Container>
   );
 };
 
